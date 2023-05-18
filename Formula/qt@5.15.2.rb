26d98f32c42a6f944d863681bdaae9698b912004
Seaphyre <33137319+seaphyre@users.noreply.github.com>
2023-05-18 15:01:35 +0000

    Update qt@5.15.2.rb

--- Formula/qt@5.15.2.rb
+++ Formula/qt@5.15.2.rb
@@ -1,6 +1,6 @@
 # Patches for Qt must be at the very least submitted to Qt's Gerrit codereview
 # rather than their bug-report Jira. The latter is rarely reviewed by Qt.
-class QtAT5 < Formula
+class QtAT5152 < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
   url "https://download.qt.io/official_releases/qt/5.15/5.15.2/single/qt-everywhere-src-5.15.2.tar.xz"
